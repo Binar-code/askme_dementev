3450a92723976ae5aed033ab371d337181a13a63
Binar <yardemyd@gmail.com>
2024-10-27 12:54:25 +0300

    Views added

--- askme/app/views.py
+++ askme/app/views.py
@@ -1,16 +1,44 @@
 from django.http import HttpResponse
-from django.shortcuts import render
 from django.template.response import TemplateResponse
+from dataclasses import dataclass
+
+
+@dataclass
+class Card:
+    img: str
+    title: str
+    text: str
+    answers: int
+    tags: list
+    likes: int
+
+    def __init__(self, img, title, text, answers, tags, likes):
+        self.img = img
+        self.title = title
+        self.text = text
+        self.answers = answers
+        self.tags = tags
+        self.likes = likes
+
+
+members = {'Aboba': 100, 'ChargeZealot': 90, 'ZerG': 50}
+tags = [(f'tag_{i}', 'tag') for i in range(1, 11)]
+
+
+def paginate(obj, req, per_page = 10):
+    pass
 
 
 def index(req):
-    members = {'Aboba': 100, 'ChargeZealot': 90, 'ZerG': 50}
-    tags = {'tag 1': 'tag', 'tag 2': 'tag', 'tag 3': 'tag', 'tag 4': 'tag', 'tag 5': 'tag', 'tag 6': 'tag',
-            'tag 7': 'tag', 'tag 8': 'tag', 'tag 9': 'tag', 'tag 10': 'tag'}
     cards = [
-        ['../static/img/placeholder-pic.png', 'SomeName 1', 'Lorem Ipsum', '0', ['tag_1', 'tag_2'], 15],
-        ['../static/img/placeholder-pic.png', 'SomeName 2', 'Lorem Ipsum', '1', ['tag_1', 'tag_2'], 9],
-        ['../static/img/placeholder-pic.png', 'SomeName 3', 'Lorem Ipsum', '0', ['tag_1', 'tag_2'], 0]
+        Card('../static/img/placeholder-pic.png', 'SomeName 1',
+             'Lorem Ipsum', 1, ['tag_1', 'tag_2'], 15),
+        Card('../static/img/placeholder-pic.png', 'SomeName 2',
+             'Lorem Ipsum', 0, ['tag_1', 'tag_2'], 10),
+        Card('../static/img/placeholder-pic.png', 'SomeName 3',
+             'Lorem Ipsum', 1, ['tag_1', 'tag_2'], -6),
+        Card('../static/img/placeholder-pic.png', 'SomeName 4',
+             'Lorem Ipsum', 0, ['tag_1', 'tag_2'], 3),
     ]
     return TemplateResponse(req, 'index.html', context={'username': 'Aboba',
                                                         'userpic': '../static/img/profile-pic.png',
@@ -18,20 +46,28 @@ def index(req):
 
 
 def login(req):
-    return TemplateResponse(req, 'login.html')
+    return TemplateResponse(req, 'login.html', context={'members': members, 'tags': tags})
 
 
 def signup(req):
-    return TemplateResponse(req, 'signup.html')
+    return TemplateResponse(req, 'signup.html', context={'members': members, 'tags': tags})
 
 
 def settings(req):
-    return TemplateResponse(req, 'settings.html')
+    return TemplateResponse(req, 'settings.html', context={'members': members, 'tags': tags})
 
 
 def tag(req):
-    return TemplateResponse(req, 'tag.html')
+    return TemplateResponse(req, 'tag.html', context={'members': members, 'tags': tags})
 
 
 def hot(req):
     return HttpResponse("In progress")
+
+
+def question(req):
+    return TemplateResponse(req, 'question.html', context={'members': members, 'tags': tags})
+
+
+def ask(req):
+    return TemplateResponse(req, 'ask.html', context={'members': members, 'tags': tags})
--- askme/askme/urls.py
+++ askme/askme/urls.py
@@ -24,6 +24,8 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
     path('settings/', views.settings, name='settings'),
-    path('tag/', views.tag, name='tag'),
-    path('hot/', views.hot, name='hot')
+    path('tag/some_tag/', views.tag, name='tag'),
+    path('hot/', views.hot, name='hot'),
+    path('question/1/', views.question, name='question'),
+    path('ask/', views.ask, name='ask')
 ]
