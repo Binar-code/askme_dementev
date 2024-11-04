de4312792a096414814fad841a8afd9f3755c8e4
Binar <yardemyd@gmail.com>
2024-11-04 12:35:52 +0300

    Models added

--- askme/app/models.py
+++ askme/app/models.py
@@ -1,3 +1,80 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+MAX_TITLE_LEN = 255
+
+class QuestionManager(models.Manager):
+    def popular(self):
+        return self.oreder_by('-likes')
+
+    def new(self):
+        return self.order_by('-created_at')
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+    )
+    avatar = models.ImageField(blank=True, null=True)
+
+class Question(models.Model):
+    user = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name='questions'
+    )
+    title = models.CharField(max_length=MAX_TITLE_LEN)
+    text = models.TextField()
+    image = models.ImageField()
+    tags = models.ManyToManyField('Tag', related_name='questions')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(
+        Question,
+        on_delete=models.CASCADE,
+        related_name='answers'
+    )
+    user = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name='answers'
+    )
+    correct = models.BooleanField()
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=MAX_TITLE_LEN, unique=True)
+
+
+class QuestionLike(models.Model):
+    question = models.ForeignKey(
+        Question,
+        on_delete=models.CASCADE,
+        related_name='likes'
+    )
+    user = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        unique_together = ('user', 'question')
+
+
+class AnswerLike(models.Model):
+    answer = models.ForeignKey(
+        Answer,
+        on_delete=models.CASCADE,
+        related_name='likes'
+    )
+    user = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        unique_together = ('user', 'answer')
--- askme/app/views.py
+++ askme/app/views.py
@@ -1,12 +1,5 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.template.response import TemplateResponse
-from askme.question import Card, Question, Answer
-
-
-members = {'Aboba': 100, 'ChargeZealot': 90, 'ZerG': 50}
-tags = [(f'tag_{i}', 'tag') for i in range(1, 11)]
-username = 'Aboba'
-userpic = 'img/profile-pic.png'
 
 
 def paginate(obj_list, req, per_page=4):
@@ -22,14 +15,6 @@ def paginate(obj_list, req, per_page=4):
 
 
 def index(req):
-    cards = [
-        Card('img/placeholder-pic.png', f'SomeTitle {i + 1}',
-             'Lorem Ipsum', ['tag_1', 'tag_2'], 15, 1)
-        for i in range(11)
-    ]
-
-    paginated_cards = paginate(cards, req, 4)
-
     return TemplateResponse(req, 'index.html', context={'username': username,
                                                         'userpic': userpic, 'members': members, 'tags': tags,
                                                         'cards': paginated_cards})
@@ -50,26 +35,12 @@ def settings(req):
 
 
 def tag(req):
-    cards = [
-        Card('img/placeholder-pic.png', f'SomeTitle {i + 1}',
-             'Lorem Ipsum', ['some_tag', 'tag_2'], 15, 1)
-        for i in range(7)
-    ]
-
-    paginated_cards = paginate(cards, req, 4)
-
     return TemplateResponse(req, 'tag.html', context={'members': members, 'tags': tags,
                                                       'userpic': userpic, 'username': username,
                                                       'cards': paginated_cards})
 
 
 def hot(req):
-    cards = [
-        Card('img/placeholder-pic.png', f'SomeTitle {i + 1}',
-             'Lorem Ipsum', ['tag_1', 'tag_2'], 999, 999)
-        for i in range(10)
-    ]
-    paginated_cards = paginate(cards, req, 4)
     return TemplateResponse(req, 'hot.html', context={'members': members, 'tags': tags,
                                                       'userpic': userpic,
                                                       'username': username,
@@ -77,18 +48,6 @@ def hot(req):
 
 
 def question(req):
-    main_question = Question('img/placeholder-pic.png', f'SomeTitle',
-             'Lorem Ipsum',  ['tag_1', 'tag_2'], 10)
-    answers = [
-        Answer('../static/img/placeholder-pic.png', f'SomeTitle {i + 1}',
-             'Lorem Ipsum',  ['tag_1', 'tag_2'], 1, False)
-        for i in range(10)
-    ]
-
-    answers[0].correct = True
-
-    paginated_answers = paginate(answers, req, 3)
-
     return TemplateResponse(req, 'question.html', context={'members': members, 'tags': tags,
                                                            'userpic': userpic, 'username': username,
                                                            'cards': paginated_answers, 'question': main_question})
